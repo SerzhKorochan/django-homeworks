6d9abd6f1b690f00ed938c8b89eda483c064d916
SerzhKorochan <serzhkorochan@gmail.com>
2022-04-11 17:58:08 +0300

    add sorting and make views of products

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import Phone
 
 
 def index(request):
@@ -7,11 +8,25 @@ def index(request):
 
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    phones = Phone.objects.all()
+    order_option = request.GET.get('sort')
+
+    if order_option == 'name':
+        phones = phones.order_by('name')
+    elif order_option == 'min_price':
+        phones = phones.order_by('price')
+    elif order_option == 'max_price':
+        phones = phones.order_by('-price')
+
+    context = {
+        'phones': phones
+    }
     return render(request, template, context)
 
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {
+        'phone': Phone.objects.get(slug=slug)
+    }
     return render(request, template, context)
