6d779fbf7f05d99c6be90204094a04dfa62c2dfc
SerzhKorochan <serzhkorochan@gmail.com>
2022-04-14 17:43:17 +0300

    change relations for articles app db and add posibility to add scopes

--- 2.2-databases-2/m2m-relations/articles/admin.py
+++ 2.2-databases-2/m2m-relations/articles/admin.py
@@ -1,13 +1,38 @@
+from pyexpat import model
 from django.contrib import admin
+from django.forms import BaseInlineFormSet, ValidationError
 
-from .models import Article, Scope
+from .models import Article, Scope, ArticleScope
 
 
-@admin.register(Scope)
-class ScopeAdmin(admin.ModelAdmin):
-    pass
+class ArticleScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        main_scopes_cnt = 0
+
+        for form in self.forms:
+            if form['is_main'].data == True:
+                main_scopes_cnt += 1
+                #from.cleaned_data.get('is_main')
+        
+        if main_scopes_cnt == 0:
+            raise ValidationError('Укажите основной раздел')
+        elif main_scopes_cnt > 1:
+            raise ValidationError('Основным может быть только один раздел')
+
+        return super().clean()
+
+
+class ArticleScopeInline(admin.TabularInline):
+    model = ArticleScope
+    formset = ArticleScopeInlineFormset
+    extra = 0
 
 
 @admin.register(Article)
 class ArticleAdmin(admin.ModelAdmin):
-    pass
+    inlines = [ArticleScopeInline,]
+
+
+@admin.register(Scope)
+class ScopeAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tag', ]
--- 2.2-databases-2/m2m-relations/articles/models.py
+++ 2.2-databases-2/m2m-relations/articles/models.py
@@ -3,25 +3,38 @@ from django.db import models
 
 
 class Article(models.Model):
-
     title = models.CharField(max_length=256, verbose_name='Название')
     text = models.TextField(verbose_name='Текст')
     published_at = models.DateTimeField(verbose_name='Дата публикации')
     image = models.ImageField(null=True, blank=True, verbose_name='Изображение',)
 
+    def __str__(self):
+        return self.title
+
     class Meta:
         verbose_name = 'Статья'
         verbose_name_plural = 'Статьи'
-
-    def __str__(self):
-        return self.title
+        ordering = ['-published_at']
 
 
 class Scope(models.Model):
     tag = models.CharField(max_length=40, verbose_name='Разделы')
-    articles = models.ManyToManyField(Article, related_name='scopes')
+    articles = models.ManyToManyField(Article, through='ArticleScope', related_name='scopes')
+
+    def __str__(self) -> str:
+        return self.tag
+
+    class Meta:
+        verbose_name = 'Раздел'
+        verbose_name_plural = 'Разделы'
+
+
+class ArticleScope(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE, verbose_name = 'Статья')
+    scope = models.ForeignKey(Scope, on_delete=models.CASCADE, verbose_name = 'Раздел')
     is_main = models.BooleanField(verbose_name='Основной')
 
     class Meta:
         verbose_name = 'Тематики статьи'
         verbose_name_plural = 'Тематики статей'
+        ordering = ['-is_main']
