3bfc1337f3ec990cb74bd3971995f5b78f55bf66
SerzhKorochan <serzhkorochan@gmail.com>
2022-04-26 13:03:12 +0300

    add oportunity to get stocks where selected product located

--- 3.2-crud/stocks_products/logistic/views.py
+++ 3.2-crud/stocks_products/logistic/views.py
@@ -1,9 +1,16 @@
-import imp
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.filters import SearchFilter
 from logistic.models import Product, Stock
 from logistic.serializers import ProductSerializer, StockSerializer
-from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
+import django_filters
+
+
+class StockFilter(django_filters.rest_framework.FilterSet):
+    products = django_filters.CharFilter(field_name='positions__product__title', lookup_expr='icontains')
+
+    class Meta:
+        model = Stock
+        fields = ['products']
 
 
 class ProductViewSet(ModelViewSet):
@@ -16,5 +23,5 @@ class ProductViewSet(ModelViewSet):
 class StockViewSet(ModelViewSet):
     queryset = Stock.objects.all()
     serializer_class = StockSerializer
-    filter_backends = [DjangoFilterBackend]
-    filterset_fields = ['products']
+    filter_backends = [django_filters.rest_framework.DjangoFilterBackend]
+    filterset_class = StockFilter
