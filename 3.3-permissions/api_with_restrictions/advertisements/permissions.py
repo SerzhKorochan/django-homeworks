ed0ed3bb62c380588c8cc91827ce44c25ca1e719
SerzhKorochan <serzhkorochan@gmail.com>
2022-04-27 19:56:16 +0300

    add all rights for superusers

--- 3.3-permissions/api_with_restrictions/advertisements/permissions.py
+++ 3.3-permissions/api_with_restrictions/advertisements/permissions.py
@@ -1,10 +1,10 @@
 from rest_framework import permissions
 
 
-class IsOwnerOrReadOnly(permissions.BasePermission):
+class IsAdminOrOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.method in permissions.SAFE_METHODS:
+        if (request.user.is_superuser) or (request.method in permissions.SAFE_METHODS) :
             return True
 
         return obj.creator == request.user
--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,10 +1,11 @@
+from ast import Is
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
 from .filters import AdvertisementFilter
-from .permissions import IsOwnerOrReadOnly
+from .permissions import IsAdminOrOwnerOrReadOnly
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -13,7 +14,7 @@ class AdvertisementViewSet(ModelViewSet):
     filterset_class = AdvertisementFilter
 
     def get_permissions(self):
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+        if self.action in ["create", "update", "partial_update", 'destroy']:
+            return [IsAuthenticated(), IsAdminOrOwnerOrReadOnly()]
             
         return []
