bfc123ecd6cf2d96dec795570fec789d3cae3de8
SerzhKorochan <serzhkorochan@gmail.com>
2022-04-27 18:37:09 +0300

    add throttling and permissions for users

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -4,6 +4,7 @@ from rest_framework.viewsets import ModelViewSet
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
 from .filters import AdvertisementFilter
+from .permissions import IsOwnerOrReadOnly
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -12,7 +13,7 @@ class AdvertisementViewSet(ModelViewSet):
     filterset_class = AdvertisementFilter
 
     def get_permissions(self):
-        """Получение прав для действий."""
         if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+            return [IsAuthenticated(), IsOwnerOrReadOnly()]
+            
         return []
