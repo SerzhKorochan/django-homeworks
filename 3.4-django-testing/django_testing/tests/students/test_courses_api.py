6ad4336eb00c7e7763bf9d55d32e188f2d565bdd
SerzhKorochan <serzhkorochan@gmail.com>
2022-05-04 21:13:53 +0300

    add tests for course list and filter by id

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -37,3 +37,29 @@ def test_course_retrieve(api_client, course_url, course_factory):
     assert len(resp_json) == 1
     recieved_course = resp_json[0]
     assert course.name == recieved_course.get('name')
+
+@pytest.mark.django_db
+def test_course_list(api_client, course_url, course_factory):
+    courses = course_factory(_quantity = 10)
+
+    resp = api_client.get(course_url)
+    resp_json = resp.json()
+
+    assert len(resp_json) == 10
+
+    given_names = [course.name for course in courses]
+    recieved_names = [course.get('name') for course in resp_json]
+
+    assert given_names == recieved_names
+
+@pytest.mark.django_db
+def test_filter_by_id(api_client, course_url, course_factory):
+    courses = course_factory(_quantity = 10)
+    selected_course = courses[0]
+
+    resp = api_client.get(course_url, data={'id': selected_course.id})
+    resp_json = resp.json()
+
+    assert len(resp_json) == 1
+    filtered_course = resp_json[0]
+    assert selected_course.id == filtered_course.get('id')
