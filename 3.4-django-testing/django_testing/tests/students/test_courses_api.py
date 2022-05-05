f4a6707f7fbad1b4690a070905ca945c582aafc4
SerzhKorochan <serzhkorochan@gmail.com>
2022-05-05 16:51:38 +0300

    add tests for filtering by name, updating and deleting

--- 3.4-django-testing/django_testing/tests/students/test_courses_api.py
+++ 3.4-django-testing/django_testing/tests/students/test_courses_api.py
@@ -32,9 +32,12 @@ def test_course_retrieve(api_client, course_url, course_factory):
     course = course_factory()
 
     resp = api_client.get(course_url)
-    resp_json = resp.json()
 
+    assert resp.status_code == 200
+
+    resp_json = resp.json()
     assert len(resp_json) == 1
+
     recieved_course = resp_json[0]
     assert course.name == recieved_course.get('name')
 
@@ -43,13 +46,14 @@ def test_course_list(api_client, course_url, course_factory):
     courses = course_factory(_quantity = 10)
 
     resp = api_client.get(course_url)
-    resp_json = resp.json()
 
+    assert resp.status_code == 200
+
+    resp_json = resp.json()
     assert len(resp_json) == 10
 
     given_names = [course.name for course in courses]
     recieved_names = [course.get('name') for course in resp_json]
-
     assert given_names == recieved_names
 
 @pytest.mark.django_db
@@ -58,8 +62,61 @@ def test_filter_by_id(api_client, course_url, course_factory):
     selected_course = courses[0]
 
     resp = api_client.get(course_url, data={'id': selected_course.id})
-    resp_json = resp.json()
 
+    assert resp.status_code == 200
+
+    resp_json = resp.json()
     assert len(resp_json) == 1
+
     filtered_course = resp_json[0]
     assert selected_course.id == filtered_course.get('id')
+
+@pytest.mark.django_db
+def test_filter_by_name(api_client, course_url, course_factory):
+    courses = course_factory(_quantity=10)
+    selected_course = courses[0]
+    the_same_courses_names = [course.name for course in courses if course.name == selected_course.name]
+
+    resp = api_client.get(course_url, data = {'name': selected_course.name})
+
+    assert resp.status_code == 200
+
+    resp_json = resp.json()
+    assert len(the_same_courses_names) == len(resp_json)
+
+    recieved_course_names = [course['name'] for course in resp_json]
+    assert the_same_courses_names == recieved_course_names
+
+@pytest.mark.parametrize('course_name', ['test1', 'test2', 'test3'])
+@pytest.mark.django_db
+def test_course_create(api_client, course_url, course_name):
+    resp = api_client.post(course_url, data={'name': course_name})
+
+    assert resp.status_code == 201
+
+    resp_json = resp.json()
+    created_course = Course.objects.get(id=resp_json['id'])
+    assert course_name == created_course.name
+
+@pytest.mark.parametrize('new_course_name', ['updated_name1', 'updated_name2'])
+@pytest.mark.django_db
+def test_course_update(api_client, course_url, course_factory, new_course_name):
+    course = course_factory()
+    url = course_url + f"{course.id}/"
+
+    resp = api_client.patch(url, data = {'name': new_course_name})
+
+    assert resp.status_code == 200
+
+    updated_course = Course.objects.get(id=course.id)
+    assert updated_course.name == new_course_name
+
+@pytest.mark.django_db
+def test_course_delete(api_client, course_url, course_factory):
+    course = course_factory()
+    url = course_url + f"{course.id}/"
+
+    resp = api_client.delete(url)
+
+    assert resp.status_code == 204
+    
